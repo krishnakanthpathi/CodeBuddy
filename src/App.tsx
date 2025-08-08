2d06d36606ef2c07549c29184186ebf7dc885400
krishna <krishnakanthpathi@gmail.com>
2025-08-08 20:51:03 +0530

    change nav response to user login in

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
@@ -9,18 +10,29 @@ import Footer from "./components/footer";
 import Login from './components/Auth/Login';
 import Signup from './components/Auth/Signup';
 
-
+interface User {
+  id: string;
+  username: string;
+  password: string;
+}
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
+  const [user, setUser] = useState<User | null>(null);
+  const UserProps = {
+    isAuthenticated,
+    user,
+    setUser,
+    setIsAuthenticated
+  };
   return (
     <>
-        <Navbar />
+        <Navbar {...UserProps} />
 
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/create" element={<CreateSnippet />} />
           <Route path="/snippets" element={<Snippets />} />
-          {/* <Route path="/snippets/:id" element={<Snippets />} /> */}
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
         </Routes>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,40 @@
 import {Link} from 'react-router-dom';
 
+interface User {
+    id: string;
+    username: string;
+    password: string;
+}
 
-function Navbar() {
+interface UserProps {
+    isAuthenticated: boolean;
+    user: User | null;
+    setUser: (user: User | null) => void;
+    setIsAuthenticated: (isAuthenticated: boolean) => void;
+}
+
+function Navbar(props: UserProps) {
+    const { isAuthenticated, user, setUser, setIsAuthenticated } = props;
     return (
         <nav className=" flex justify-between text-black p-2 rounded-lg m-4 shadow-lg">
             <div className="p-2">
                 <Link to="/">Code Buddy</Link>
             </div>
             <ul className="flex space-x-4 mt-2">
-                <li><Link to="/snippets">My Snippets</Link></li>
-                <li><Link className="button bg-yellow-500 text-white p-2 rounded" to="/create">Create Snippet</Link></li>
-                <li><Link className="button bg-green-500 text-white p-2 rounded" to="/login">Log in</Link></li>
-                <li><Link className="button bg-blue-500 text-white p-2 rounded" to="/signup">Sign up</Link></li>
+                {isAuthenticated && 
+                    <li><Link className='button bg-blue-500 text-white p-2 rounded' to="/snippets">My Snippets</Link></li>}
+
+                {isAuthenticated &&
+                    <li><Link className="button bg-yellow-500 text-white p-2 rounded" to="/create">Create Snippet</Link></li>}
+
+                {isAuthenticated ? (
+                    <li><Link className="button bg-red-500 text-white p-2 rounded" to="/logout">Log out</Link></li>
+                ) : (
+                    <li><Link className="button bg-green-500 text-white p-2 rounded" to="/login">Log in</Link></li>
+                )}
+                {!isAuthenticated && (
+                    <li><Link className="button bg-blue-500 text-white p-2 rounded" to="/signup">Sign up</Link></li>
+                )}
             </ul>
         </nav>
     );
