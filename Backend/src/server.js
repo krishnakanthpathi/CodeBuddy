074580e9f442b19db6fc222176588b371d23f479
Pathi Krishna Kanth <krishnakanth@Pathis-MacBook-Air.local>
2025-09-02 09:42:53 +0530

    added sql connection

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,16 +1,31 @@
 
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import pool from './config/db.js';
 
 dotenv.config();
 
 const app = express();
+
+app.use(cors())
+app.use(express.json());
 
 const PORT = process.env.PORT || 3000 ;
 
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
+});
+
+app.get('/db', async (req, res) => {
+    try {
+        const [rows] = await pool.query('SELECT NOW() AS time');
+        res.json({ solution: rows[0].time });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ "error": 'Database query failed' , "details": error.message});
+    }
 });
 
 
